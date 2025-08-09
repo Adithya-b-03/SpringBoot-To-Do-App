2c5c1d2fe7a6d888042279c317626225afd57c10
Adithya-b-03 <adithyaakash0304@gmail.com>
2025-08-09 19:16:32 +0530

    Add Spring Boot project files

--- src/main/java/adithya/demo/Todo/models/Todo.java
+++ src/main/java/adithya/demo/Todo/models/Todo.java
@@ -3,7 +3,10 @@ package adithya.demo.Todo.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.NotFound;
 
 @Entity
 @Data
@@ -11,6 +14,8 @@ public class Todo {
     @Id
     @GeneratedValue
     Long id;
+    @NotBlank
+    @NotNull
     String title;
     String description;
     boolean isCompleted;
